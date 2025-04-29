ae92ea5bb71943261123325d8d99b4ea21da18d3
Dionisio <ceduardodfernandes@gmail.com>
2025-04-29 17:33:27 -0300

    feat: fixes logout

--- src/auth/AuthContext.tsx
+++ src/auth/AuthContext.tsx
@@ -42,7 +42,7 @@ export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
   const logout = () => {
     localStorage.removeItem('jwt')
     setUser(null)
-    window.location.href = '/login'
+    window.location.href = '/#login'
   }
 
   useEffect(() => {
